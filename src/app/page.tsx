be9708f6fea7690aa23ca171a80a2cc846332cf7
Kate <yea90@ukr.net>
2026-02-12 14:12:04 +0200

    add lock screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/home/hero";
 import HowWork from "@/components/home/how-work";
 import AboutSection from "@/components/home/about";
 import { getQuestions } from "@/helpers/get-questions";
+import { sora } from "./fonts";
 
 
 export const metadata = {
@@ -15,11 +16,17 @@ export default async function Home() {
   
   return (
     <>
-      <Hero/>
+       <div className="fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center bg-linear-to-r from-(--accent) from-0% to-(--secondary) to-70%"> 
+        <div className={`text-(--bg-dark) ${sora.className} text-6xl font-bold text-center`}>
+          <p>QodeQuiz</p>
+          <p className="mt-5">Coming soon</p>
+        </div>
+      </div>
+      {/* <Hero/>
       <span className="sectionDivider"></span>
       <HowWork/>
       <span className="sectionDivider"></span>
-      <AboutSection/>
+      <AboutSection/> */}
     </>
     
   )
